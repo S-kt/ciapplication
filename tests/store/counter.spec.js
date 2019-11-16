c4608fadbcbc73296bb38d8da144d7a540203b43
saraaltuwaijri <saratw93@gmail.com>
2019-11-16 16:11:49 +0300

    check type COUNTER_INCREMENT

--- tests/store/counter.spec.js
+++
@@ -1,8 +0,0 @@
-import {
-    COUNTER_INCREMENT,
-    default as counter
-  } from 'modules/counter'
-
-  it('Should return an action with type "COUNTER_INCREMENT".', () => {
-    expect(increment()).to.have.property('type', COUNTER_INCREMENT)
-  })
